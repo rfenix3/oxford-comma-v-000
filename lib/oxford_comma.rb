e076f94c681997e6b5dfe2c43c6c689fde24e904
Richard Fenix <richard.fenix@gmail.com>
2018-05-26 03:32:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 #array = ["fiddleheads","okra","kohlrabi"]
 #array = ["okra","kohlrabi"]
 array = ["kohlrabi"]
+=======
+array = ["fiddleheads","okra","kohlrabi"]
+>>>>>>> 6afefa7f49acd7c88ecff8bdbbfd649340cb2fc4
 
 def oxford_comma(array)
   case array.length
@@ -14,7 +18,12 @@ def oxford_comma(array)
       array.join(", ")
     else
       lastElement = array.pop
+<<<<<<< HEAD
       lastElement = "and #{lastElement}"
+=======
+      secondToLast = array.pop
+      lastElement = "#{secondToLast} and #{lastElement}"
+>>>>>>> 6afefa7f49acd7c88ecff8bdbbfd649340cb2fc4
       array.push(lastElement)
       array.join(", ")
   end
