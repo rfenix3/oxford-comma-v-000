9e40bdd3bec82566951cd71dbab6590169356351
Richard Fenix <richard.fenix@gmail.com>
2018-05-26 03:24:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+#array = ["fiddleheads","okra","kohlrabi"]
+#array = ["okra","kohlrabi"]
+array = ["kohlrabi"]
 
+def oxford_comma(array)
+  case array.length
+    when 1 
+      array.join
+    when 2
+      lastElement = array.pop
+      secondToLast = array.pop
+      lastElement = "#{secondToLast} and #{lastElement}"
+      array.push(lastElement)
+      array.join(", ")
+    else
+      lastElement = array.pop
+      lastElement = "and #{lastElement}"
+      array.push(lastElement)
+      array.join(", ")
+  end
 end
+
+puts oxford_comma(array).inspect
